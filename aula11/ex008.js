994ccb3fe9f4b0affde9756cf137968d7c23d632
Fabricio-Lemos <fabriciosantana349@gmail.com>
2020-12-15 14:19:51 -0300

    Alterações 3

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,5 +1,5 @@
 var vel = 78.2
 console.log(`Sua velocidade é ${vel}`)
 if (vel > 60) {
-    console.log(`A velocidade Permitida foi ultrapassada/. Foi Multado ;)`)
+    console.log(`A velocidade Permitida foi ultrapassada. Foi Multado ;)`)
 }
